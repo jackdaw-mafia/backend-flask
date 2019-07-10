b2b5ec78e7de5d36c2f27bd2c871f0afab832702
miraematt <matttootell@hotmail.com>
2019-07-10 10:27:02 +0100

    python files removed

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello, world!", 200
+    response = {"body": "Hello, world!"}
+    return json.dumps(response)
 
 
 # We only need this for local development.
